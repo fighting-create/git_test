505cb42d51170606620ed5f719f3364b64f93102
Xin <654034573@qq.com>
2020-10-14 23:06:34 +0800

    版本3

--- src/hello_git.java
+++ src/hello_git.java
@@ -6,5 +6,6 @@ public class hello_git {
     public static void main(String[] args) {
         System.out.println("git版本1");
         System.out.println("git版本2");
+        System.out.println("git版本3");
     }
 }
